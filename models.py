9cab072b3238a7c1c0973ab02b38df5ee894b7af
Bruno Mortella <bmortella@gmail.com>
2017-12-11 08:44:00 -0200

    Nova db

---
+++ models.py
@@ -0,0 +1,36 @@
+import peewee, datetime
+from playhouse.fields import ManyToManyField
+
+db = peewee.SqliteDatabase('data.db')
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+class Player(BaseModel):
+
+    discord_id = peewee.CharField()
+    total_kills = peewee.IntegerField()
+
+
+class Win(BaseModel):
+ 
+    date = peewee.DateTimeField(default=datetime.datetime.now)
+    total_kills = peewee.IntegerField()
+
+class Winner(BaseModel):
+
+    win = peewee.ForeignKeyField(Win, related_name="winners", null=False)
+    player = peewee.ForeignKeyField(Player, related_name="wins")
+    kills = peewee.IntegerField()
+
+
+if __name__ == '__main__':
+    try:
+        db.create_tables([Player, Win, Winner])
+    except Exception:
+        pass
+    ###
+    pla = Player.create(discord_id="656565", total_kills=2)
+    win = Win.create()
+    winp = Winner.create(win=win, player=pla, kills=2)
